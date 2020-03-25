23360c712c5e8b7a907ed470959abc8c2eb0c0e0
insungsong <song22861@naver.com>
2020-03-25 12:22:15 +0900

    fukin Weather App With React

---
+++ App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Alert } from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
+import Weather from "./Weather";
+
+const API_KEY = "d38f9b675379bcddc8dd93951f918c5e";
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeater = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
+    );
+
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+  getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeater(latitude, longitude);
+    } catch (error) {
+      Alert.alert("본인의 위치를 찾을 수 없습니다.", "다시 시도해주세요");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather condition={condition} temp={Math.round(temp)} />
+    );
+  }
+}
